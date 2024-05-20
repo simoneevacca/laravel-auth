3f8b739bac312f94e016884cb3a23e7f73e6f0b9
simoneevacca <v.simone.vacca@gmail.com>
2024-05-20 15:39:31 +0200

    update route in navbar from url() in route()

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -67,8 +67,8 @@
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ url('admin') }}">{{__('Dashboard')}}</a>
-                                <a class="dropdown-item" href="{{ url('profile') }}">{{__('Profile')}}</a>
+                                <a class="dropdown-item" href="{{ route('admin.dashboard') }}">{{__('Dashboard')}}</a>
+                                <a class="dropdown-item" href="{{ route('profile.edit') }}">{{__('Profile')}}</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                     {{ __('Logout') }}
