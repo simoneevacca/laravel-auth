7218784d4e16db81e07c6abc7d31c3fada899dea
simoneevacca <v.simone.vacca@gmail.com>
2024-05-23 13:06:08 +0200

    seeding types table

--- database/migrations/2024_05_23_103110_add_type_id_foreing_key_to_projects_table.php
+++ database/migrations/2024_05_23_103110_add_type_id_foreing_key_to_projects_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('projects', function (Blueprint $table) {
-            $table->unsignedBigInteger('type_id')->after('id')->nullable();
-            $table->foreign('type_id')->references('id')->on('tipes')->onDelete('set null');
+            $table->unsignedBigInteger('type_id')->nullable()->after('id');
+            $table->foreign('type_id')->references('id')->on('types')->onDelete('set null');
         });
     }
 
--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Type;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,6 +14,13 @@ class TypeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $types = ['front-end', 'back-end', 'full-stack'];
+
+        foreach($types as $type) {
+            $newType = new Type();
+            $newType->type_name = $type;
+            $newType->slug = Str::slug($newType->type_name);
+            $newType->save();
+        }
     }
 }
