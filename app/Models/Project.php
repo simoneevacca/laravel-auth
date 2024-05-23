33da7aec530026c0fdbd9d406f2efeddc569c976
simoneevacca <v.simone.vacca@gmail.com>
2024-05-23 12:45:30 +0200

    ake relationship between Project and Type models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,5 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+    public function types(){
+        return $this->belongsTo(Type::class);
+    }
+
     protected $fillable = ['project_name', 'slug', 'description', 'preview_image', 'link_view', 'link_code'];
 }
